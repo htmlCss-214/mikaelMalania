058e28ddd71729eb27788f313b371fe2a54612c8
Mikael Malania <m.malania@noxtton.com>
2024-09-11 21:53:01 +0400

    feat: added lesson files

---
+++ lesson13Offline/scripts/firstScript.js
@@ -0,0 +1,9 @@
+// console.log(typeof 'Hello, World!', typeof "Hello, World!", "jake's dad bought him a new bike");
+console.log(typeof 123, typeof 123.123)
+console.log(typeof {})
+console.log(typeof [])
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof Symbol('symbol'))
+console.log(typeof new Date())
